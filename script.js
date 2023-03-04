33f8509c347ec0310a822cf2db8e0e1f29ce8e7a
Mohamed Saeed <117910216+JS-MSaeed@users.noreply.github.com>
2023-03-04 22:51:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const citiesContainer = document.querySelector(".city__parent");
+//SELECT ELEMENT IN HTML
+const citiesContainer = document.querySelector(".city__parent"); 
 
+//FUNCTION TO RENDER VIEW
 const renderData = function (data) {
   const html = `
     <option>${data}</option>
@@ -9,8 +11,9 @@ const renderData = function (data) {
   citiesContainer.insertAdjacentHTML("afterbegin", html);
 };
 
+//FETCH FUNCTION
 const fetchData = async function () {
-  const res = await fetch(`./cities.json`);
+  const res = await fetch(`./cities.json`);         // FILE FROM LOCAL MACHINE
   const data = await res.json();
   const results = data.forEach((ele) => {
     renderData(ele.cityNameAr);
